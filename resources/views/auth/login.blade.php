089d044acc8800593ea63baf965c21df6f70c1bd
nemoto <haikonggenben@gmail.com>
2026-03-07 18:00:11 +0900

    a
    aa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,22 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
+<div class="flex items-center justify-end mt-4">
+    @if (Route::has('register'))
+        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 mr-4" href="{{ route('register') }}">
+            {{ __('新規登録はこちら') }}
+        </a>
+    @endif
+    @if (Route::has('password.request'))
+        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+            {{ __('Forgot your password?') }}
+        </a>
+    @endif
 
+    <x-primary-button class="ms-3">
+        {{ __('Log in') }}
+    </x-primary-button>
+</div>
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
