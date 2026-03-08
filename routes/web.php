ee3aae3e4f8f6b309bec4920722722bbffee1416
nemoto <haikonggenben@gmail.com>
2026-03-08 09:46:33 +0900

    syuusei

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::resource('events', EventController::class)->only([
-    'index', 'create', 'store', 'show', 'edit', 'update', 'destroy'
-]);
+Route::get('/members', [MemberController::class, 'index'])->name('members.index');
+Route::get('/events', [EventController::class, 'index'])->name('events.index');
+Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
+Route::post('/events', [EventController::class, 'store'])->name('events.store');
+Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
+Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
+Route::put('/events/{event}', [EventController::class, 'update'])->name('events.update');
+Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
 
 require __DIR__.'/auth.php';
